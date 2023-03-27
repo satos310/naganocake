dabba9045a9783c2c7ec8c7fe1a7bc14d9f91ec7
satos310 <119788681+satos310@users.noreply.github.com>
2023-03-27 15:24:58 +0000

    [add column=>migration]nagano

--- db/migrate/20230325175054_devise_create_custemers.rb
+++ db/migrate/20230325175054_devise_create_custemers.rb
@@ -4,8 +4,16 @@ class DeviseCreateCustemers < ActiveRecord::Migration[6.1]
   def change
     create_table :custemers do |t|
       ## Database authenticatable
+      t.string :last_name,          null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name_kana,     null: false, default: ""
+      t.string :first_name,         null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :postal_code,        null: false, default: ""
+      t.string :address,            null: false, default: ""
+      t.string :telephone_number,   null: false, default: ""
+      t.boolean :is_deleted,        null: false, default: FALSE
 
       ## Recoverable
       t.string   :reset_password_token
