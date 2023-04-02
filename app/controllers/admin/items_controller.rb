e73dab2d37a9e137b58d995923abaa91b94303ed
satos310 <119788681+satos310@users.noreply.github.com>
2023-04-02 17:08:41 +0000

    [fix admin/items/new, admin/items/index]nagano

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,18 @@
 class Admin::ItemsController < ApplicationController
+
   def index
+    @item = Item.all
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
@@ -16,5 +22,14 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+
+  protect_from_forgery
+
+  private
+
+  def item_params
+    # genre_id? or genre_id_id?　どっちか
+    params.require(:item).permit(:genre_id_id, :genre_id, :name, :introduction, :price, :is_active)
   end
 end
