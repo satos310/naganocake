f425539b84c87245254668520aca0eb882457ac6
satos310 <119788681+satos310@users.noreply.github.com>
2023-04-13 14:20:15 +0000

    [fix customer/edit]nagano

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,12 +1,25 @@
 class Public::CustomersController < ApplicationController
+
   def show
     @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+    if @customer == current_customer
+      render "edit"
+    else
+      redirect_to customer_path(current_customer)
+    end
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer)
+    else
+      render "edit"
+    end
   end
 
   def unsubscribe
