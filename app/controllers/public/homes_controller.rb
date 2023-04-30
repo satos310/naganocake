8534be4beaee30d5cd59b20d5438ab667a446f45
satos310 <119788681+satos310@users.noreply.github.com>
2023-04-30 14:04:34 +0000

    [fix bootstrap]nagano

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::HomesController < ApplicationController
 
   def top
     # 商品を新着順に4件取得
-    @items = Item.all.limit(4)
+    @items = Item.all.limit(4).order(created_at: :desc)
   end
 
   def about
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,6 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.where(is_active: true).page(params[:page]).per(8)
+    @items = Item.where(is_active: true).page(params[:page]).per(8).order(created_at: :desc)
   end
 
   def show
